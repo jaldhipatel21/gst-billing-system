228aa90b5feffb30d4c64fd0bf12dd5c57756e9a
jaldhipatel21 <jaldhipatel2110@gmail.com>
2026-07-18 13:01:12 +0530

    Configure production API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
-// Automatically attach JWT token
-API.interceptors.request.use(
-    (config) => {
 
-        const token = localStorage.getItem("token");
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
+  return config;
+});
+
 export default API;
